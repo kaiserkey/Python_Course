3aca6e8ff08597ad0e4dc50c6a3ba356eb743eca
DanielSan <kaiserkey2@gmail.com>
2023-03-02 19:35:04 -0300

    modificacion de archivo github

--- ejercicio99.py
+++ ejercicio99.py
@@ -1,4 +1,106 @@
 """ 
+
+    detectar cambios en archivos de una carpeta con python
+
+    Para detectar cambios en los archivos de una carpeta con Python, 
+    puedes usar el módulo os.walk. Esta función recorrerá toda la carpeta 
+    y sus subcarpetas y devolverá la lista de todos los archivos y 
+    carpetas que están dentro.
+
+    Puedes entonces recorrer la lista de archivos y comparar los tiempos 
+    de modificación de cada archivo con los tiempos de modificación anteriores. 
+    Si hay algún cambio en los tiempos de modificación, entonces sabrás que algo 
+    cambió en el archivo desde la última vez que se revisó.
+
+    Aquí hay un ejemplo de cómo puedes implementar esto en Python:
+
+    import os
+
+    # Ruta de la carpeta que queremos revisar
+    ruta = '/home/usuario/carpeta/'
+
+    # Obtenemos la lista de archivos y carpetas dentro de la carpeta
+    archivos_actuales = os.listdir(ruta)
+
+    # Obtenemos los tiempos de modificación de los archivos
+    tiempos_actuales = {archivo:os.stat(ruta+archivo).st_mtime for archivo in archivos_actuales}
+
+    # Guardamos los tiempos de modificación de los archivos en un archivo
+    with open('tiempos_anteriores.txt', 'w') as archivo:
+        for archivo, tiempo in tiempos_actuales.items():
+            archivo.write('{},{}\n'.format(archivo, tiempo))
+
+    # Revisamos la carpeta cada cierto tiempo
+    while True:
+        archivos_nuevos = os.listdir(ruta)
+        tiempos_nuevos = {archivo:os.stat(ruta+archivo).st_mtime for archivo in archivos_nuevos}
+
+        # Recorremos los nuevos tiempos de modificación
+        for archivo, tiempo in tiempos_nuevos.items():
+            if archivo in tiempos_actuales:
+                # Si el archivo ya existía, revisamos si el tiempo de modificación ha cambiado
+                if tiempo != tiempos_actuales[archivo]:
+                    print('El archivo {} ha cambiado desde la última revisión'.format(archivo))
+            else:
+                # Si el archivo es nuevo, mostramos un mensaje
+                print('Se ha encontrado un nuevo archivo: {}'.format(archivo))
+
+        # Actualizamos los tiempos de modificación
+        tiempos_actuales = tiempos_nuevos
+
+        # Esperamos antes de revisar nuevamente
+        time.sleep(60)
+
+    detectar cambios en archivos locales con pygithub en python
+
+    Para detectar cambios en los archivos locales con PyGithub en Python, 
+    primero necesitas crear un repositorio en Github. Una vez creado el repositorio, 
+    puedes usar el método Repository.get_commits para obtener la lista de los últimos 
+    commits realizados.
+
+    Para cada commit, puedes usar el método Commit.get_files para obtener la 
+    lista de archivos modificados. Una vez que tengas la lista de archivos, 
+    puedes recorrerla para obtener las URLs de los archivos y descargarlos en 
+    tu computadora. Luego, puedes usar alguna librería para comparar los archivos 
+    descargados con los archivos locales y determinar si hay alguna diferencia.
+
+    Aquí hay un ejemplo de cómo puedes implementar esto en Python:
+
+    import requests
+    from github import Github
+
+    # Autenticación en Github
+    g = Github("username", "password")
+
+    # Obtenemos el repositorio
+    repo = g.get_repo("username/repository")
+
+    # Obtenemos los últimos commits
+    commits = repo.get_commits()
+
+    # Recorremos cada commit
+    for commit in commits:
+        # Obtenemos la lista de archivos modificados
+        archivos = commit.get_files()
+
+        # Recorremos cada archivo
+        for archivo in archivos:
+            # Obtenemos la URL del archivo
+            url = archivo.raw_url
+
+            # Descargamos el archivo
+            r = requests.get(url)
+            data = r.text
+
+            # Aquí puedes usar alguna librería para comparar el archivo descargado con el archivo local y determinar si hay alguna diferencia.
+
+
+
+
+
+
+
+
     Automatizar la subida de cambios nuevos a github con python
 
     Puede automatizar la subida de cambios nuevos a GitHub con Python usando la biblioteca PyGithub. Esta biblioteca le permite conectarse a su cuenta de GitHub, examinar los repositorios, crear nuevos repositorios, crear ramas, crear pulgares, enumerar archivos, y mucho más. Esta biblioteca le permite automatizar la subida de cambios nuevos al repositorio de GitHub mediante el uso de métodos lógicos y loops.
--- github_automatic.py
+++ github_automatic.py
@@ -1,6 +1,49 @@
-from github import Github
+""" from github import Github
 g = Github("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")
 
 # Then play with your Github objects:
 for repo in g.get_user().get_repos():
-    print(repo.name)
+    print(repo.name) """
+    
+import os, time
+
+path = os.getcwd()# Ruta de la carpeta que queremos revisar
+
+def timesOfArchives(path):
+    # Obtenemos la lista de archivos y carpetas dentro de la carpeta
+    current_archives= os.listdir(path)
+    # Obtenemos los tiempos de modificación de los archivos en un diccionario
+    return {archive:os.stat(f"{path}/{archive}").st_mtime for archive in current_archives}
+    """ 
+        List Comprehension
+        [item: <expr> for <elemento> in <lista>]
+
+        Donde <expr> es una expresión que se evalúa para cada elemento de la lista <lista>. 
+        El resultado de <expr> se asigna al elemento con nombre <item>.
+    """
+current_times = timesOfArchives(path)
+
+# Guardamos los tiempos de modificación de los archivos en un archivo
+with open('previous_times.txt', 'w') as archive:
+    for archives, times in current_times.items():
+        archive.write(f'{archives},{times}\n')
+
+
+""" # Revisamos la carpeta cada cierto tiempo
+while True:
+    current_archives= os.listdir(path)
+    current_times = {archive:os.stat(f"{path}/{archive}").st_mtime for archive in current_archives}
+    time.sleep(59) """
+    
+    
+"""
+# Recorremos los nuevos tiempos de modificación
+for archive, tiempo in current_times.items():
+    if archive in current_times:
+        # Si el archivo ya existía, revisamos si el tiempo de modificación ha cambiado
+        if tiempo != current_times[archive]:
+            print(f'El archivo {archive} ha cambiado desde la última revisión')
+    else:
+        # Si el archivo es nuevo, mostramos un mensaje
+        print(f'Se ha encontrado un nuevo archivo: {archive}') """
+
