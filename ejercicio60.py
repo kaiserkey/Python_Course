9635eb72076ec6eecf8912694f0ddb26a3496107
kaiserkey <kaiserkey2@gmail.com>
2023-03-03 17:36:32 -0300

    new changes

--- ejercicio60.py
+++ ejercicio60.py
@@ -12,4 +12,13 @@ M = np.zeros((3,3), dtype=int)
 
 for i in range(len(M)):
     for j in range(len(M[0])):
-        print(M[i][j]
+        print(M[i][j], end=" ")
+    print()
+
+# crear una copia de la matriz
+copy_M = M.copy()
+
+for i in range(len(copy_M)):
+    for j in range(len(copy_M[0])):
+        print(copy_M[i][j], end=" ")
+    print()
