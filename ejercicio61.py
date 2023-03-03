79e1136da33ea2038853bada000159ca2d4bca83
kaiserkey <kaiserkey2@gmail.com>
2023-03-03 17:56:27 -0300

    new changes

--- ejercicio61.py
+++ ejercicio61.py
@@ -1,4 +1,13 @@
 """ 
     Crear matriz con numpy e introducir numeros por el usuario
 """
+import numpy as np
 
+
+
+M = np.empty((4,4), dtype=int)
+
+print("Matriz de 4x4")
+for i in range(len(M)):
+    for j in range(len(M[0])):
+        M[i][j] = int(input("Ingrese un valor: "))
