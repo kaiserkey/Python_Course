b12165a1efca74c5e98c849ccac5b14c67c57771
kaiserkey <kaiserkey2@gmail.com>
2023-03-03 18:05:06 -0300

    new changes

--- ejercicio62.py
+++ ejercicio62.py
@@ -1,9 +1,13 @@
 """ 
     realizar la multiplicacion de dos matrices con numpy
 """
+import numpy as np
 
 f = int(input("Ingrese el numero de filas de la matriz A: "))
 c = int(input("Ingrese el numero de columnas de la matriz A: "))
 cb = int(input("Ingrese el numero de columnas de la matriz B: "))
 
+A = np.empty((f,c), dtype=int)
+B = np.empty((c,cb), dtype=int)
+C = np.zero((f,cb), dtype=int)
 
