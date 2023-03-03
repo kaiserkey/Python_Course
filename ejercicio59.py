f441c64d174e792bd664809efce5e38f916e0fc0
kaiserkey <kaiserkey2@gmail.com>
2023-03-03 16:50:35 -0300

    new changes

--- ejercicio59.py
+++ ejercicio59.py
@@ -4,4 +4,11 @@
 
 M = [[0 for x in range(4)] for y in range(4)]
 
-print(0 for x in range(4) )
+for i in range(len(M)):
+    for j in range(len(M[0])):
+        M[i][j] = int(input("Ingrese un valor: "))
+        
+for i in range(len(M)):
+    for j in range(len(M[0])):
+        print(M[i][j], end=" ")
+    print()
