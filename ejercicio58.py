74459f272c16ca57ac3ff15b98cf6899340fbda7
kaiserkey <kaiserkey2@gmail.com>
2023-03-03 16:43:42 -0300

    new changes

--- ejercicio58.py
+++ ejercicio58.py
@@ -1,5 +1,9 @@
 """ 
     realizar la division de dos matrices de 3x3
     
-    para poder dividir dos matrices, deben ser del mismo tamaño
+    para poder dividir dos matrices, deben ser del mismo tamaño y
+    el resultado de la division de dos matrices es una matriz de nxm
+    se debe dividir cada elemento de la primera matriz por cada 
+    elemento de la segunda matriz
+    la formula es C[i][j] = A[i][j] / B[i][j]
 """
