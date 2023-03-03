20fa013b933211f6d27595352f6ae6f0e8288b50
kaiserkey <kaiserkey2@gmail.com>
2023-03-03 16:19:35 -0300

    new changes

--- ejercicio55.py
+++ ejercicio55.py
@@ -1,3 +1,11 @@
 """ 
-    Realizar la suma de dos
+    Realizar la suma de dos matrices de 3x3
 """
+
+A = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+B = [[2, 3, 9], [5, 6, 2], [2, 2, 7]]
+
+f = len(A)
+c = len(A[0])
+
+if
