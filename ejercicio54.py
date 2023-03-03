0d221dead365a294e80d7a194aa408e2ac4ed3c5
kaiserkey <kaiserkey2@gmail.com>
2023-03-03 16:03:08 -0300

    new changes

--- ejercicio54.py
+++ ejercicio54.py
@@ -1,8 +1,8 @@
 """ 
-    Convertir los numeros del 0 al 30 en una lista y mostrarlos
+    Convertir los numeros impares del 0 al 30 en una lista y mostrarlos
 """
 
-lista = list(range(0,30))
+lista = list(range(1,30, 2))
 
-for index in range(0,31):
+for index in range(0,30):
     print(lista[index])
