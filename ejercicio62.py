93851b1173216ed6ffb93d3f63163e09c9cf8ea2
kaiserkey <kaiserkey2@gmail.com>
2023-03-03 18:08:29 -0300

    new changes

--- ejercicio62.py
+++ ejercicio62.py
@@ -9,7 +9,7 @@ cb = int(input("Ingrese el numero de columnas de la matriz B: "))
 
 A = np.empty((f,c), dtype=int)
 B = np.empty((c,cb), dtype=int)
-C = np.zero((f,cb))
+C = np.zeros((f,cb))
 
 print(f"==== Matriz A de {f}x{c} ====")
 for i in range(f):
@@ -23,5 +23,5 @@ for i in range(c):
 
 C = A * B
 
-print()
+print(f"==== Matriz C de {f}x{cb} ====")
 print(C)
