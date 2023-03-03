7b3892eb1ae01d92740816dc5070b8abd00414d1
kaiserkey <kaiserkey2@gmail.com>
2023-03-03 16:29:13 -0300

    new changes

--- ejercicio57.py
+++ ejercicio57.py
@@ -4,6 +4,7 @@
     Para poder multiplicar dos matrices, el numero de columnas de la primera matriz
     debe ser igual al numero de filas de la segunda matriz.
     y el resultado de la multiplicacion de dos matrices es una matriz de nxm
-    
+    la formula es la siguiente:
+    C[i][j] = A[i][0] * B[0][j] + A[i][1] * B[1][j] + A[i][2] * B[2][j]
 """
 
