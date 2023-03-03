2f08bf06ecf907a92b0e70c3779581cc5ec1cf01
kaiserkey <kaiserkey2@gmail.com>
2023-03-03 16:44:52 -0300

    new changes

--- ejercicio58.py
+++ ejercicio58.py
@@ -7,3 +7,18 @@
     elemento de la segunda matriz
     la formula es C[i][j] = A[i][j] / B[i][j]
 """
+
+A = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+B = [[2, 3, 9], [5, 6, 2], [2, 2, 7]]
+
+f = len(A)
+c = len(A[0])
+
+if len(A) == len(B) & len(A[0]) == len(B[0]):
+    C = [[0 for x in range(c)] for y in range(f)]
+    
+    for i in range(f):
+        for j in range(c):
+            C[i][j] = A[i][j] / B[i][j]
+
+for 
