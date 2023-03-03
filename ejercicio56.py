842450d6cb2bdf39d10f46fa27a9a0f6cea9dade
kaiserkey <kaiserkey2@gmail.com>
2023-03-03 16:25:36 -0300

    new changes

--- ejercicio56.py
+++ ejercicio56.py
@@ -1,3 +1,23 @@
 """ 
-    realizar la resta de 
+    realizar la resta de dos matrices de 3x3
 """
+
+A = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+B = [[2, 3, 9], [5, 6, 2], [2, 2, 7]]
+
+f = len(A)
+c = len(A[0])
+
+if len(A) == len(B) & len(A[0]) == len(B[0]):
+    C = [[0 for x in range(c)] for y in range(f)]
+    
+    for i in range(f):
+        for j in range(c):
+            C[i][j] = A[i][j] - B[i][j]
+else:
+    print("Las matrices no son del mismo tamaño")
+
+for i in range(f):
+    for j in range(c):
+        print(C[i][j], end=" ")
+    print()
