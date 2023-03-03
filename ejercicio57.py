31f38f88f3b6f14a1d7ec7988aacb464cb407000
kaiserkey <kaiserkey2@gmail.com>
2023-03-03 16:28:03 -0300

    new changes

--- ejercicio57.py
+++ ejercicio57.py
@@ -2,5 +2,8 @@
     realizar la multiplicacion de dos matrices de 3x3
     
     Para poder multiplicar dos matrices, el numero de columnas de la primera matriz
+    debe ser igual al numero de filas de la segunda matriz.
+    y el resultado de la multiplicacion de dos matrices es una matriz de nxm
+    
 """
 
