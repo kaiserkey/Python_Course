1e029b2b628ffee3963a166c7266cc50715f1e8f
kaiserkey <kaiserkey2@gmail.com>
2023-03-03 15:57:09 -0300

    new changes

--- ejercicio53.py
+++ ejercicio53.py
@@ -1,5 +1,6 @@
 """ 
     conversion a listas
-    Se puede transformar cualquier objeto iterable a una lista c
+    Se puede transformar cualquier objeto iterable a una lista con la funcion list()
 """
 
+print()
