a7d4e86f316ecd9ede421daaf0a7b462999a6fa3
kaiserkey <kaiserkey2@gmail.com>
2023-03-05 23:04:51 -0300

    new changes

--- ejercicio64.py
+++ ejercicio64.py
@@ -2,4 +2,6 @@
     Vamos a solicitar al usuario 8 numeros enteros del 0 al 9.
     A continuacion, con esos numeros calcularemos la letra correspoondiente
     y la guardaremos en una variable. Finalmente, crearemos un diccionario
+    con dos claves, cada una guardara, respectivamete, los numeros y la letra del DNI. 
+    FInalmente, mostraremos el diccionario por pantalla.
 """
