0b21da8a54fc67eec38283e7728182b3abf67bfe
kaiserkey <kaiserkey2@gmail.com>
2023-03-05 23:03:43 -0300

    new changes

--- ejercicio64.py
+++ ejercicio64.py
@@ -1,3 +1,5 @@
 """ 
-    Vamos a solicitar al usuario 8 numeros enteros del 0 al 9. 
+    Vamos a solicitar al usuario 8 numeros enteros del 0 al 9.
+    A continuacion, con esos numeros calcularemos la letra correspoondiente
+    y la guardaremos en una variable. Finalmente, crearemos un diccionario
 """
