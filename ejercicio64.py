b568bd7602b8e223e5dd5f1a66abf50ddb58110c
kaiserkey <kaiserkey2@gmail.com>
2023-03-05 23:15:54 -0300

    new changes

--- ejercicio64.py
+++ ejercicio64.py
@@ -15,5 +15,11 @@ dni = 0
 for i in range(len(numeros)):
     # calculamos el DNI con los numeros introducidos por el usuario
     dni += 10 ** (len[numeros] - i - 1) * numeros[i]
-    # este calculo es equivalente a:
+
+letras = { 0: "T", 1: "R", 2: "W", 3: "A", 
+            4: "G", 5: "M", 6: "Y", 7: "F", 
+            8: "P", 9: "D", 10: "X", 11: "B", 
+            12: "N", 13: "J", 14: "Z", 15: "S", 
+            16: "Q", 17: "V", 18: "H", 19: "L", 20: "C", 21: "K", 22: "E" }
+# calculamos el resto de la division entre el DNI y 23
     
