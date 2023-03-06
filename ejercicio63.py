3f2d6b4e3b2ea0cdf6bb29495b8e19e7bf64beff
kaiserkey <kaiserkey2@gmail.com>
2023-03-05 22:17:28 -0300

    new changes

--- ejercicio63.py
+++ ejercicio63.py
@@ -1,8 +1,18 @@
 """ 
-    Diccionarios en python: son estructuras de datos que almacenan datos en pares clave:valor.
+    Los diccionarios en Python son una estructura de datos que se utiliza para 
+    almacenar y organizar información de manera eficiente. Están compuestos de 
+    pares de clave-valor, donde cada clave se asocia con un valor que se puede 
+    usar para recuperar la información. Esto significa que los diccionarios 
+    permiten acceder a los datos de forma rápida y eficiente sin necesidad de 
+    recorrer la lista completa de elementos. Esta es una de las principales 
+    razones por las que los diccionarios son tan útiles para el procesamiento 
+    de datos en Python.
 """
 
 myDiccionary = { "nombre": "Juan", "apellido": "Perez", "edad": 25, "cursos": ["Python", "Django", "JavaScript"] }
 
-print()
+print(myDiccionary)
 print(myDiccionary["cursos"])
+
+# Se puede convertir un iterable en un diccionario con la funcion dict()
+
