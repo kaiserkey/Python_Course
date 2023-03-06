da150d3ace6cf81f377b967b191130263320334e
kaiserkey <kaiserkey2@gmail.com>
2023-03-06 20:39:07 -0300

    new changes

--- ejercicio67.py
+++ ejercicio67.py
@@ -9,8 +9,11 @@ dicc.clear()
 print(dicc)
 
 # el metodo copy() devuelve una copia del diccionario
+dicc = { "animal": "perro", "edad": 5, "raza": "pastor aleman" }
 dicc_copy = dicc.copy()
 print(dicc_copy)
 
 # el metodo fromkeys() crea un nuevo diccionario con las claves especificadas y el valor especificado
 dicc = dict.fromkeys(["animal", "edad", "raza"], "desconocido")
+print(dicc)
+
