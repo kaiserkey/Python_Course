7f17417e60f56fc290f55da6a4e163a6ff0f9c6a
kaiserkey <kaiserkey2@gmail.com>
2023-03-06 19:21:59 -0300

    new changes

--- ejercicio66.py
+++ ejercicio66.py
@@ -1,3 +1,4 @@
 """ 
-    Tenemos un diccionario con 5 claves: Math, English, History
+    Tenemos un diccionario con 5 claves: Math, English, History, Geography, Biology. Cada clave
+    contiene una lista con 3 notas. El valor de cada clave es la media de las notas de la lista.
 """
