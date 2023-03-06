c3bcd312d72eee68f3bd30437326143d49ea39c0
kaiserkey <kaiserkey2@gmail.com>
2023-03-06 20:36:51 -0300

    new changes

--- ejercicio67.py
+++ ejercicio67.py
@@ -4,3 +4,12 @@
 
 # el metodo clear() elimina todos los elementos del diccionario
 dicc = { "animal": "perro", "edad": 5, "raza": "pastor aleman" }
+print(dicc)
+dicc.clear()
+print(dicc)
+
+# el metodo copy() devuelve una copia del diccionario
+dicc_copy = dicc.copy()
+print(dicc_copy)
+
+# el metodo fromkeys() crea un nuevo diccionario con las claves especificadas y el valor especificado
