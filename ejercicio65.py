ea1632f4f0ce1eefd9865c05965208d68f08bbf3
kaiserkey <kaiserkey2@gmail.com>
2023-03-05 23:57:21 -0300

    new changes

--- ejercicio65.py
+++ ejercicio65.py
@@ -2,3 +2,8 @@
     Podemos seguir usando un bucle for para recorrer un diccionario, pero en este caso
     no necesitamos usar la funcion range() para obtener los indices de los elementos
 """
+
+dicc = { "animal": "perro", "edad": 5, "raza": "pastor aleman" }
+
+for key in dicc:
+    print(key, ":", dicc[key])
