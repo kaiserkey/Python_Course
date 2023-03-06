c89061849162f3d1e4b36707ccc0bec6031efe2c
kaiserkey <kaiserkey2@gmail.com>
2023-03-05 23:56:13 -0300

    new changes

---
+++ ejercicio65.py
@@ -0,0 +1,4 @@
+""" 
+    Podemos seguir usando un bucle for para recorrer un diccionario, pero en este caso
+    no necesitamos usar la funcion range() para obtener los indices de los elementos
+"""
