e5fe3a297f6276bca9020b172053b15bddd92569
kaiserkey <kaiserkey2@gmail.com>
2023-03-05 22:15:13 -0300

    new changes

--- ejercicio63.py
+++ ejercicio63.py
@@ -1,3 +1,5 @@
 """ 
     Diccionarios en python: son estructuras de datos que almacenan datos en pares clave:valor.
 """
+
+myDiccionary = { "nombre": "Juan", "apellido": "Perez", "edad": 25, "cursos": ["Python", "Django", "JavaScript"] }
