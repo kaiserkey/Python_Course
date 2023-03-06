b0edddda89e8a83591e98f79741fa4f5c7f117d8
kaiserkey <kaiserkey2@gmail.com>
2023-03-06 19:24:14 -0300

    new changes

--- ejercicio66.py
+++ ejercicio66.py
@@ -1,5 +1,10 @@
 """ 
     Tenemos un diccionario con 5 claves: Math, English, History, Geography, Biology. Cada clave
     contiene una lista con 3 notas. EL usuario debe introducir cada nota por teclado y luego para cada clave
-    se debe mostrar la media
+    se debe mostrar la media de las notas. 
 """
+
+courses = { "Math": [], "English": [], "History": [], "Geography": [], "Biology": [] }
+
+for key in courses:
+    for 
