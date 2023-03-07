f90997c5f8d40778f94e29898098499fe7f890ac
kaiserkey <kaiserkey2@gmail.com>
2023-03-06 21:56:37 -0300

    new changes

---
+++ ejercicio70.py
@@ -0,0 +1,19 @@
+#6.  Crear un diccionario con 3 elementos de la siguiente forma:
+myDictionary =  {
+                    'equipo1': {
+                                    'nombre': 'Real Madrid',
+                                    'ciudad': 'Madrid'
+                                },
+                    'equipo2': {
+                                    'nombre': 'Barcelona',
+                                    'ciudad': 'Barcelona'
+                                },
+                    'equipo3': {
+                                    'nombre': 'Valencia',
+                                    'ciudad': 'Valencia'
+                                }
+                }
+
+#7.  Recorrer todos los elementos del diccionario creado en el ejercicio 6 imprimiendo cada elemento.
+
+for 
