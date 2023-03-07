8134951b230d54d1ee7dd90c0a33284ef43e805f
kaiserkey <kaiserkey2@gmail.com>
2023-03-06 21:43:04 -0300

    new changes

--- ejercicio68.py
+++ ejercicio68.py
@@ -2,4 +2,9 @@
     leer un string por teclado y devolver un diccionario con la cantidad de apariciones de cada caracter en el string
 """
 
-myString = input("Ingrese un: ")
+myString = input("Ingrese una frase: ")
+
+
+
+for char in myString:
+    if char not in 
