09cdef2144da39fd7d5538d7e39107269a09c07d
kaiserkey <kaiserkey2@gmail.com>
2023-03-06 21:50:54 -0300

    new changes

---
+++ ejercicio69.py
@@ -0,0 +1,9 @@
+#1.  Crear un diccionario con 5 elementos de la siguiente forma:
+ 
+   {
+    'nombre': 'Juan',
+    'edad': 20,
+    'ciudad': 'Madrid',
+    'pais': 'España',
+    'profesión': 'programador'
+    }
