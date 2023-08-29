2e7b6ab105135f471576048b3083cb45760370e7
kaiserkey <kaiserkey2@gmail.com>
2023-08-29 20:42:26 -0300

    new changes

---
+++ PythonMicrosoft/ejercicio2.py
@@ -0,0 +1,12 @@
+
+sum = 1+2
+product = sum * 2
+print(f"La suma es: {sum}, el producto: {product}")
+
+print("show this in the console")
+
+#tipos de datos
+planets_in_solar_system = 8 # int, pluto used to be the 9th planet, but is too small
+distance_to_alpha_centauri = 4.367 # float, lightyears
+can_liftoff = True
+shuttle_landed_on_the_moon = "Apollo 11" #string 
