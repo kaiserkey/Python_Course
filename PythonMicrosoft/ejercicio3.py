6e635d67583de5c347dcf3f7239bf25244b4ebfe
kaiserkey <kaiserkey2@gmail.com>
2023-08-30 18:23:48 -0300

    new changes

---
+++ PythonMicrosoft/ejercicio3.py
@@ -0,0 +1,11 @@
+# formas de pasar datos al programa
+import sys
+
+print(sys.argv)
+print(sys.argv[0]) # program name
+print(sys.argv[1]) # first arg
+
+# mediante input
+print("Welcome to the greeter program")
+name = input("Enter your name: ")
+print("Greetings " + name)
