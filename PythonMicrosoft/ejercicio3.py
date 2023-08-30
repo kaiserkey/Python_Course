0cf2d1554dbe2e40d011c7a40722075a9118f9a7
kaiserkey <kaiserkey2@gmail.com>
2023-08-30 18:29:44 -0300

    new changes

--- PythonMicrosoft/ejercicio3.py
+++ PythonMicrosoft/ejercicio3.py
@@ -10,4 +10,8 @@ print("Welcome to the greeter program")
 name = input("Enter your name: ")
 print("Greetings " + name)
 
+print("calculator program")
+first_number = input("first number: ")
+second_number = input("second number: ")
+print(first_number + second_number)
 
