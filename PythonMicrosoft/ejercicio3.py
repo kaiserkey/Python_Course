24826f91d770415d2626ae53352dcdfc4b7a6545
kaiserkey <kaiserkey2@gmail.com>
2023-08-30 18:30:01 -0300

    new changes

--- PythonMicrosoft/ejercicio3.py
+++ PythonMicrosoft/ejercicio3.py
@@ -13,5 +13,5 @@ print("Greetings " + name)
 print("calculator program")
 first_number = input("first number: ")
 second_number = input("second number: ")
-print(first_number + second_number)
+print(int(first_number) + int(second_number))
 
